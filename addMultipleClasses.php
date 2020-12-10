dbf72d42062506684479c62b56539688bf66e50c
njwaugh <56400991+njwaugh@users.noreply.github.com>
2020-12-09 23:57:32 -0500

    Additional Selenium Test
    added a test for adding multiple classes to the calendar

---
+++ addMultipleClasses.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once('vendor/autoload.php');
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Chrome\ChromeDriver;
+
+$web_driver = RemoteWebDriver::create("http://localhost:4444/wd/hub",
+    array("platform"=>"WINDOWS", "browserName"=>"chrome"), 120000);
+$web_driver->get("http://localhost/tspProject/");
+
+$crnSearch = $web_driver->findElement(WebDriverBy::name("crn"));
+if($crnSearch) {
+    $crnSearch->sendKeys("82488");
+    $crnSearch->submit();
+}
+
+$crnSearch = $web_driver->findElement(WebDriverBy::name("crn"));
+if($crnSearch) {
+    $crnSearch->sendKeys("82488");
+    $crnSearch->submit();
+}
+
+$crnSearch = $web_driver->findElement(WebDriverBy::name("crn"));
+if($crnSearch) {
+    $crnSearch->sendKeys("82491");
+    $crnSearch->submit();
+}
+
+$crnSearch = $web_driver->findElement(WebDriverBy::name("crn"));
+if($crnSearch) {
+    $crnSearch->sendKeys("82491");
+    $crnSearch->submit();
+}
+
+$crnSearch = $web_driver->findElement(WebDriverBy::name("crn"));
+if($crnSearch) {
+    $crnSearch->sendKeys("82507");
+    $crnSearch->submit();
+}
+
+$crnSearch = $web_driver->findElement(WebDriverBy::name("crn"));
+if($crnSearch) {
+    $crnSearch->sendKeys("82507");
+    $crnSearch->submit();
+}
+
+print $web_driver->getTitle();
+
+?>
